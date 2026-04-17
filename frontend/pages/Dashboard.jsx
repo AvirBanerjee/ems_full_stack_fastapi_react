43063565c6e88d2df39bb4bfc85bc793a715735d
ashutosh <ashutoshbanergee939@gmail.com>
2026-04-17 20:57:34 +0530

    editpage also added

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -5,12 +5,14 @@ import { useState,useEffect } from 'react'
 function Dashboard() {
     const[data,setData] =useState(null)
     useEffect(()=>{
-        API.get("/dashboard")
+        API.get("/dashboard").then(res=>setData(res.data),[])
     })
+    if(!data) return <h1>Loading...</h1>
   return (
     <div>
       <Navbar/>
-      <h1>{}</h1>
+      <h1>{data.fullname}</h1>
+      <p>Total Employs:{data.total_employs}</p>
     </div>
   )
 }
